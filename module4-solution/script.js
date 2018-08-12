90e52603de09decff2de53c8728bc7ef5bdf3bb7
Marta <41085675+s-marta@users.noreply.github.com>
2018-08-12 21:18:05 +0200

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,1 +1,26 @@
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Solution
+
+// STEP 1:
+(function () {
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // STEP 10:
+  for (var i = 0; i < names.length; i++) {
+
+    // STEP 11:
+     var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // STEP 12:
+    if (firstLetter == 'j') {
+      byeSpeaker.speak(names[i])
+    } else {
+      helloSpeaker.speak(names[i])
+    }
+  }
+})();
 
